d257975251780c532446639e8882beed361e818c
Jiabaokang <jbksy@foxmail.com>
2023-02-20 15:29:12 +0800

    首次提交，验证ffmpeg合并视频

---
+++ android/app/src/main/kotlin/com/jbk/merge_video/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.jbk.merge_video
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,16 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import ffmpeg_kit_flutter_min_gpl
+import package_info_plus
+import path_provider_foundation
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FFmpegKitFlutterPlugin.register(with: registry.registrar(forPlugin: "FFmpegKitFlutterPlugin"))
+  FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+}
